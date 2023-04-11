f581869ca57a1af708a072fdff8f2362c309211c
stan-raynell <dunn2000@mail.ru>
2023-04-11 13:09:06 +0300

    Model tuning

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,16 +1,13 @@
 class Rate < ApplicationRecord
   validates :value, presence: true, format: { with: /\d+\,?(\d+)?/ }
 
-  # after_create_commit -> {
-  #                       broadcast_replace_to "rates",
-  #                                            target: "rates",
-  #                                            partial: "rates/rate",
-  #                                            locals: { rate: self }
-  #                     }
   def self.get_rate
     @rate = Rate.find_by(id: 1) || Rate.new
     @rate.value = Nokogiri::XML(URI.open("http://www.cbr.ru/scripts/XML_daily.asp")).
       xpath('//Valute[@ID="R01235"]/Value').text
-    @rate.save
+    if DateTime.current > Rate.last.end_date
+      @rate.save
+      Turbo::StreamsChannel.broadcast_update_to "rates", target: "rates", partial: "rates/rate", locals: { rate: @rate }
+    end
   end
 end
