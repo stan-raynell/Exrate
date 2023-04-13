ac4a3092f8332f528a72571b46e13be401f17b62
stan-raynell <dunn2000@mail.ru>
2023-04-13 10:34:49 +0300

    Final touch

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -2,7 +2,8 @@
 
 class RatesController < ApplicationController
   def index
-    @rate = Rate.last || Rate.new(value: "---", end_date: 5.minutes.ago)
+    Rate.get_rate if !Rate.last
+    @rate = Rate.last
     return unless Rate.where(id: 1).present? && (DateTime.current > @rate.end_date)
 
     @rate = Rate.find(1)
